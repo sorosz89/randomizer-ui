1b9bc6e4b3d41030812d90ae370f473c55c4f443
Sandor_Orosz <sandor_orosz@epam.com>
2022-03-26 08:54:50 +0100

    add e2e tests

---
+++ e2e/example.spec.js
@@ -0,0 +1,15 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('http://randomizer-ui.s3-website.eu-central-1.amazonaws.com/');
+});
+
+
+test.describe('Open Randomizer UI', () => {
+  test('should open landing page', async ({ page }) => {
+    const logo = page.locator('.App-logo')
+    await expect(logo).toBeVisible();
+  });
+
+})
